815d268b7f2e3d7880aaf8c9ca75c3494fcf1c84
MumuAkay <mumu.akay@gmail.com>
2022-06-03 15:35:50 +0300

    commit login system

---
+++ patients/urls.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from django.urls import path, include
+from App import views
+
+
+urlpatterns = [
+    # Native path to access the django admin
+    path('admin/', admin.site.urls),
+    # Path to access the frontend page
+    path('',views.frontend, name="frontend"),
+    
+    # Path to login / logout
+    path('login/', include('django.contrib.auth.urls')),
+    
+    #-----------------------------BACKEND SECTION-----------------------|
+    
+    # Path to access the backend page
+    path('backend/',   views.backend,          name="backend"),
+    # url on browser   function in views.py    url inside the template.html 
+]
